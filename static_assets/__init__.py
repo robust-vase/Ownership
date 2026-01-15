1ac032131f9081eb20a5471f203975c32c7e3136
robust-vase <1416718784@qq.com>
2026-01-15 19:30:12 +0800

    Ownership

--- static_assets/__init__.py
+++
@@ -1,7 +0,0 @@
-# Static assets module - reusable UI components
-from .ui_components import (
-    render_common_css,
-    render_left_panel_html,
-    render_right_panel_html,
-    render_core_script
-)
